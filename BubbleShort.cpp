de42e5faae9688c8bfbbb97bfbc1b9912c1d704f
Marchjul <marchellogiovani28@gmail.com>
2026-03-05 17:51:54 +0700

    input elemen array

--- BubbleShort.cpp
+++ BubbleShort.cpp
@@ -22,6 +22,15 @@ void input()
 
     cout << endl;
     cout << "==============" << endl;
+    cout <<"Masukan Elemen Array" << endl;
+    cout << "==============" << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Data ke-" << (i+i) << ": ";
+        cin >> arr[i];
+    }
 }
+
 
 
